02093094508fb6ae31f230cfecc25e65f70da6e9
timdavidcole <timdavidcole@gmail.com>
2019-06-21 11:58:07 +0100

    finished exercise 9

---
+++ directory9.rb
@@ -0,0 +1,54 @@
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    if students.count > 1
+      puts "Now we have #{students.count} students"
+      name = gets.chomp
+    else
+      puts "Now we have 1 student"
+      name = gets.chomp
+    end
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)"
+  end
+end
+
+def print_footer(names)
+  if names.count > 1
+    puts "Overall, we have #{names.count} great students"
+  else
+    puts "We have 1 great student"
+  end
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
